05b5316d34a1189fa00a65585ac04fb546b0735d
Mgamboa <marianellag400@gmail.com>
2023-03-18 18:47:52 -0500

    saving progress

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# current-clock
-With the help from other sources, I will be making a pm/am working clock.
+ * Source Help
+    -[flexiple](https://flexiple.com/javascript/javascript-clock/) - Had a great thurough example and guide, with step by step help of code.
+    -[geeksforgeeks](https://www.geeksforgeeks.org/how-to-design-digital-clock-using-javascript/) - a quick pseudo code list of what I should do, without the help of an actual code. It's great as if to challenge myself on what I understand of javascript, and be able to make the clock project.
+    -[w3schools](https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock) - I was able to compare to flexiple, as they were similar, only difference was on how flexiple used the meridiem on the clock, not w3school.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function currentTime() {
     let hours = today.getHours(); //method that returns the hour for a specific date, according to local time.- mdn web docs
     let minute = today.getMinutes(); // method that returns minutes
     let seconds = today.getSeconds(); // method that returns seconds
-    let time = "AM";
+    let meridiem = "AM"; //what m means, crossing the meridiem
     if (hours == 0) {
         //looking if 'is equal to' 0
         hours = 12;
@@ -13,12 +13,24 @@ function currentTime() {
     if (hours > 12) {
         hours = hours - 12;
         //its a loop!!! Once the 12th hour comes, it will be sub. by 12, starting the process until 12!!
-        time = "PM";
-
-        //condition statement: (condition ? if true : if false)
-        hours = (hours < 10) ? "0" + hours : hours; //instead of being displayed as single, it will be displayed as double digit =>
-        minute = (minute < 10) ? "0" + minute : minute; // => as long as it is less than 10
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
-        //another resource: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_comparison
+        meridiem = "PM";
     }
+
+    //condition statement: (condition ? if true : if false)
+    hours = (hours < 10) ? "0" + hours : hours; //instead of being displayed as single, it will be displayed as double digit =>
+    minute = (minute < 10) ? "0" + minute : minute; // => as long as it is less than 10
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    //another resource: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_comparison
+
+    let time = hours + ":" + minute + ":" + seconds + " " + meridiem;
+    // " " is the space between seconds and the meridiem. 
+    //concatination(?) is used to connect all 
+
+    document.getElementById('clock').innerHTML = time;
+    //either innerHTML and innerText both can pass through to html div.
+    let update = setTimeout(function () { currentTime() }, 1000);
+    //1000ms to a second.
+
 }
+
+currentTime();
