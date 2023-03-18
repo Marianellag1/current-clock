8ed09c13ff322ffe8a84cf74b0f41c42e5d2c663
Mgamboa <marianellag400@gmail.com>
2023-03-18 18:26:27 -0500

    saving progress

---
+++ script.js
@@ -0,0 +1,24 @@
+function currentTime() {
+    //creating an instance of the Date obj.- g for g
+    const today = new Date(); //When called as a constructor(constant), returns a new Date obj. - mdn web docs
+    let hours = today.getHours(); //method that returns the hour for a specific date, according to local time.- mdn web docs
+    let minute = today.getMinutes(); // method that returns minutes
+    let seconds = today.getSeconds(); // method that returns seconds
+    let time = "AM";
+    if (hours == 0) {
+        //looking if 'is equal to' 0
+        hours = 12;
+        //telling the system what hours will be.
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+        //its a loop!!! Once the 12th hour comes, it will be sub. by 12, starting the process until 12!!
+        time = "PM";
+
+        //condition statement: (condition ? if true : if false)
+        hours = (hours < 10) ? "0" + hours : hours; //instead of being displayed as single, it will be displayed as double digit =>
+        minute = (minute < 10) ? "0" + minute : minute; // => as long as it is less than 10
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        //another resource: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_comparison
+    }
+}
